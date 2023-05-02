4ba2b9f14f845550b1f28fd103affe902f398b41
Hamid Raza <58918006+hmdraza9@users.noreply.github.com>
2023-05-02 23:25:53 +0530

    Update TestProg4.java

--- src/test/java/TestAutomation/APIAutomation/TestProg4.java
+++ src/test/java/TestAutomation/APIAutomation/TestProg4.java
@@ -16,7 +16,7 @@ public class TestProg4 {
 		
 
 		
-		int array1[] = {1,5,3,6,8,5,6,3,0,9,11,23,11};
+		int array1[] = {1,5,3,6,8,5,6,3,0,9,11,23,11,98};
 		
 		List<Integer> numList = new ArrayList<Integer>();
 		for(int i : array1) {
